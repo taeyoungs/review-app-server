ba1db7ff34df55d3b02be60e81e57695fda69e17
taeyoung <xoxodudwkd@gmail.com>
2020-05-06 12:51:22 +0900

    fix error

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     methods: ['GET', 'POST'],
   }),
 );
--- src/passport.js
+++ src/passport.js
@@ -1,6 +1,5 @@
 import passport from 'passport';
 import User from './models/User';
-import NaverStrategy from 'passport-naver';
 import routes from './routes';
 
 passport.use(User.createStrategy());
